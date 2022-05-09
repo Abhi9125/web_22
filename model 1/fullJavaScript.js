9fc465c96f0b81e1c53a1d223116cdc5acb8597e
Abhi9125 <abhi.singh3132@gmail.com>
2022-05-09 15:11:30 +0530

    decimal to binary

---
+++ model 1/fullJavaScript.js
@@ -0,0 +1,26 @@
+//1. print 
+
+// <--------------------------------------->
+//2. Declare a Variable in js
+// let a = 1234;
+// console.log("value of a is "+a);
+
+
+// <--------------------------------------->
+// 3.Function in Js 
+// function sum(){
+//     let a=10,b=20;
+//     console.log("sum of a and b ="+ (a+b));
+// }
+// sum();
+// // 3.1 Function call by parameter
+// function sum(c,d){
+//     console.log("sum of c and d="+ (c+d));
+//     return c;
+// }
+// let a=20,b=30;
+// let ret = sum(a,b);
+// console.log("return"+ ret);
+
+// ------------------------------------------------->
+// 4.  Decimal to Binary
