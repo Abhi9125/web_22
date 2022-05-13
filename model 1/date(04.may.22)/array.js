dd9beb27742f9eb7f1bb4151d9c50f161b7944e8
Abhi9125 <abhi.singh3132@gmail.com>
2022-05-13 15:10:37 +0530

    lec (04-05-22)

---
+++ model 1/date(04.may.22)/array.js
@@ -0,0 +1,63 @@
+//1.--------------- 1D array
+// let a =["abhishek","akhand ","Singh","Sandeep"];
+// console.log(a);
+
+// // 2.---------------2d array---------------
+// let b = [[1,2,3],[4,5,6],[7,8,9]];
+// console.log(b);
+
+
+
+// =-----------------travesh in array----------
+
+// traverse -> 
+// let a = [10, 20, 30, 40, 50, 60];
+// for (let i = 0; i < a.length; i++) {
+//     console.log(i + " : " + a[i]);
+// }
+
+
+
+
+// 3.--------------Push/Pop on array------------------
+// let a =["abhishek","akhand ","Singh","Sandeep"];
+// a.pop();
+// console.log(a)
+// a.push("Satayam");
+// console.log(a);
+
+
+
+// 4.------------Shift/Unshift in array----------------
+// let a =["abhishek","akhand ","Singh","Sandeep"];
+// a.shift();
+// console.log(a);
+// a.unshift("abhishek");
+// console.log(a);
+
+// 5.-----------slice in array---------------------
+// let a =["abhishek","akhand ","Singh","Sandeep"];
+// let b = a.slice(1,3);
+// console.log(b); 
+  
+
+// 6.-----------indexof function in array----------
+// let a =["abhishek","akhand ","Singh","akhand","Sandeep"];
+// let b = a.indexOf("akhand");
+// console.log(b);
+
+
+// 7.-----------includes function------------------
+// let a =["abhishek","akhand ","Singh","akhand","Sandeep"];
+// // let b= a.includes("Sandeep");
+// let b= a.includes("sandeep");
+// console.log(b);
+
+// 8.-----------splice function--------------------
+let a =["abhishek","akhand ","Singh","akhand","Sandeep"];
+let b = a.splice(1,3);
+console.log(b); 
+
+
+
+
